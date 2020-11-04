88e2fe5269d1d8ba2a83d9180ff6de15599e35c4
Adam Rummer <31801759+cyclingwithelephants@users.noreply.github.com>
2020-11-04 19:03:14 +0000

    created idempotent symlink function

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
 
-for file in .zshrc .vimrc; do
-  if [ ! -L ~/${file} ]; then
-    rm -f ~/${file}
-    ln -s  ~/.config/dotfiles/${file} ~/${file}
+function idempotent_sym_link () {
+  file_location=$1 ; shift
+  link_location=$1 ; shift
+  
+  if [ -L ${link_location} ]; then
+    # if symlink does not point to file
+    if [ "$(readlink ${link_location})" != "${file_location}" ]; then
+      rm -f ${link_location}
+    fi
   fi
-done
+  
+  if [ ! -L ${link_location} ]; then
+    rm -f ${link_location}
+    ln -s  ${file_location} ${link_location}
+  fi
+}
+
+# zshrc will load all config from ~/.config/zsh/*
+idempotent_sym_link ~/.config/dotfiles/zsh/.zshrc ~/.zshrc
+
+idempotent_sym_link ~/.config/dotfiles/.vimrc ~/.vimrc
+
+# alacritty already looks in ~/.config/alacritty/alacritty.yml
