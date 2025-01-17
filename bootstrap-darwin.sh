baaed2162d508aa41906c4a1b9888318179dc299
Adam Rummer <adamrummer@gmail.com>
2025-01-17 14:04:51 +0000

    prep for MBP 2025

--- bootstrap-darwin.sh
+++ bootstrap-darwin.sh
@@ -6,28 +6,18 @@ set -eou pipefail
 echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee' visudo -f /private/etc/sudoers.d/adam
 
 # install applications using brew, brew cask and Mac App store
+BREWFILE="${HOME}/.config/brewfile/Brewfile"
+brew bundle install --file ${BREWFILE}
 # clean up applications not installed using the brewfile
-brew bundle install --file $HOME/.config/brewfile/Brewfile
-brew bundle --force cleanup --file $HOME/.config/brewfile/Brewfile
+brew bundle --force cleanup --file ${BREWFILE}
 
 # zshrc will load all config from ~/.config/zsh/* thanks to ~/.zshenv specifying ZDOTDIR
 SYSTEM_ZSHENV=/etc/zshenv
 sudo ln -sf $HOME/.config/zsh/.zshenv $SYSTEM_ZSHENV
 chmod +r $SYSTEM_ZSHENV
 
-
-# Yabai - only run on personal machine
-# https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)
-if [[ "$(which yabai)" != ""]]; then
-  echo "$(whoami) ALL = (root) NOPASSWD: $(which yabai) --load-sa" | sudo EDITOR='tee' visudo -f /private/etc/sudoers.d/yabai
-  sudo yabai --install-sa
-  sudo yabai --load-sa
-  brew services start yabai
-  brew services start skhd
-fi
-
-
-# Apple specific configuration
+##
+## Apple specific configuration
 
 sudo scutil --set HostName garlic
 
@@ -35,3 +25,13 @@ sudo scutil --set HostName garlic
 # https://apple.stackexchange.com/questions/10467/how-to-increase-keyboard-key-repeat-rate-on-os-x?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 2         # normal minimum is 2  (30  ms)
+
+## Yabai - only run on personal machine
+## https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)
+#if [[ "$(which yabai)" != ""]]; then
+#  echo "$(whoami) ALL = (root) NOPASSWD: $(which yabai) --load-sa" | sudo EDITOR='tee' visudo -f /private/etc/sudoers.d/yabai
+#  sudo yabai --install-sa
+#  sudo yabai --load-sa
+#  brew services start yabai
+#  brew services start skhd
+#fi
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -5,7 +5,7 @@ export CLICOLOR=1
 #export TERM=alacritty
 # Not strictly used in MacOS but some apps still do observe this
 export XDG_CONFIG_HOME="${HOME}/.config"
-export XDG_CACHE_HOME="${HOME}/.cache/"
+export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_STATE_HOME="${HOME}/.local/state"
 
@@ -16,11 +16,9 @@ export PATH="${HOME}/bin:/usr/local/bin:/usr/local/sbin/usr/bin:/usr/sbin:/bin:/
 
 # GOLANG
 export GOPATH="${HOME}/code/golang"
+export GOROOT="${HOME}/go"
 export GOOS="darwin"
-export GOARCH="amd64"
-export GOROOT=/usr/local/opt/go/libexec
 export GO111MODULE=on
-export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin:$HOME/bin"
 
 # Homebrew
 export HOMEBREW_CASK_OPTS="--appdir=${HOME}/Applications"
