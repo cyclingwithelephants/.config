85f8719563633c8bc93c281b8e811c919d89a718
Adam Rummer <adamrummer@gmail.com>
2025-01-20 22:12:42 +0000

    remove linux configs

--- sway/scripts/laptop-screen-open-on-closed-off.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-# https://github.com/swaywm/sway/wiki#Multihead
-laptop_screen=${1}
-if grep -q open /proc/acpi/button/lid/LID/state; then
-    swaymsg output "${laptop_screen}" enable
-else
-    swaymsg output "${laptop_screen}" disable
-fi
