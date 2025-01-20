fe8c26a0faa86416b719b5cd5fe2d4f1512a4b05
Adam Rummer <adamrummer@gmail.com>
2025-01-20 22:09:08 +0000

    add talscale GUI install script

---
+++ install_scripts/tailscale.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+tmp_dir=/tmp/install_scripts/tailscale
+tailscale_pkg="${tmp_dir}/install.pkg"
+
+if [[ "$(which tailscale)" == "" ]]; then
+  echo "installing tailscale as it has not been found on the system"
+  mkdir -p "${tmp_dir}"
+  cd "${tmp_dir}" || exit 1
+  wget https://pkgs.tailscale.com/stable/Tailscale-latest-macos.pkg -O "${tailscale_pkg}"
+  open "${tailscale_pkg}"
+else
+  echo "tailscale GUI is already installed"
+fi
