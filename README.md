4fbdcd4b2a6aedf821c0a12336873391fe9ae9d9
Adam Rummer <adamrummer@gmail.com>
2025-01-21 01:22:37 +0000

    update README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # dotfiles
+
+The contents of my .config directory.
+It includes a bootstrapping functionality for use on new machines. 
+
+## Highlights
+
+### Automated Brewfile management
+When you install/remove packages and casks using `brew [--cask] install/uninstall` the brewfile is automatically updated to reflect this change.
+
+
+This is implemented over the following files:
+- `zsh/aliases/brew.sh`
+- `zsh/functions/brew.sh`
+
+## To run
+
+```bash
+bash ./bootstrap-darwin.sh <hostname>
+```
--- bootstrap-darwin.sh
+++ bootstrap-darwin.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+hostname=shallot
+
 set -eou pipefail
 
 cd $(dirname $(readlink -f $0))
@@ -8,25 +10,22 @@ source zsh/.zshenv
 # add self to sudoers, we do this first to prevent needing to write password multiple times
 echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee' visudo -f "/private/etc/sudoers.d/$(whoami)"
 
-# install applications using brew, brew cask and Mac App store
+# install applications using brew
 if [[ "$(which brew)" == "brew not found" ]]; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle install --file "${HOMEBREW_BREWFILE}"
-# clean up applications not installed using the brewfile
-brew bundle --force cleanup --file "${HOMEBREW_BREWFILE}"
+brew bundle cleanup --file "${HOMEBREW_BREWFILE}" --force
 
 # zshrc will load all config from ~/.config/zsh/* thanks to ~/.zshenv specifying ZDOTDIR
-SYSTEM_ZSHENV=/etc/zshenv
-sudo ln -sf $HOME/.config/zsh/.zshenv $SYSTEM_ZSHENV
-chmod +r $SYSTEM_ZSHENV
+sudo ln -sf "${HOME}/.config/zsh/.zshenv" /etc/zshenv
+chmod +r /etc/zshenv
 
 ##
 ## Apple specific configuration
 
 # https://www.autodesk.com/support/technical/article/caas/sfdcarticles/sfdcarticles/Setting-the-Mac-hostname-or-computer-name-from-the-terminal.html
-hostname=shallot
 sudo scutil --set HostName "${hostname}"
 sudo scutil --set LocalHostName "${hostname}"
 sudo scutil --set ComputerName "${hostname}"
